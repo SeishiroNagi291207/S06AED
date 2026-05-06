b0e42d1c2ca7b2d94e540b2c50859a0a930346d3
The_Lazy_GeniusP29 <pieroac2912@gmail.com>
2026-05-06 12:14:38 -0500

    T08

---
+++ Assets/Scripts/Dictionary/DiccionaryExample.cs
@@ -0,0 +1,57 @@
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DictionaryExample : MonoBehaviour
+{
+    public Dictionary<int, string> pokemones = new();
+
+
+    void Start()
+    {
+        pokemones.Add(1, "Bulbasur");
+        pokemones.Add(354, "Bannete");
+        pokemones.Add(85, "Dodrio");
+
+
+        if (pokemones.TryAdd(25, "Bulbasur"))
+        {
+            Debug.Log("exito");
+        }
+        else
+        {
+            Debug.Log("error");
+        }
+
+        Debug.Log(pokemones[63]);
+
+
+        if (pokemones.TryGetValue(85, out string x))
+        {
+            Debug.Log(x);
+        }
+        else
+        {
+            Debug.Log("error");
+        }
+
+
+    }
+
+
+    void Update()
+    {
+
+    }
+    /*
+    [Button]
+    public void GetHashing()
+    {
+        int LargoDic = 10;
+        string x = "PedroPablo";
+
+        Debug.Log(x.GetHashCode());
+
+        Debug.Log("Su indice seria: " + (Mathf.Abs(x.GetHashCode()) % LargoDic));//->blockchain :D
+    }*/
+}
